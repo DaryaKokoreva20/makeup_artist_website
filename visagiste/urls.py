e0dce4635187d6fffe2d4f3aace98e68719092d0
Daria Kokoreva <kokorewa.dashenka@yandex.ru>
2024-11-20 18:20:12 +0300

    Names of apps and their view-functions are added to the documentation.txt

--- visagiste/urls.py
+++ visagiste/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('homapage.urls')),     
+    path('services/', include('services.urls')),
+    path('booking/', include('booking.urls')),
+    path('reviews/', include('reviews.urls')),
+    path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
 ]
